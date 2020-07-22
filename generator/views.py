761615656f3258cb9feeb43394f63eb9b1389ca4
JARVIS2002 <57238453+JARVIS2002@users.noreply.github.com>
2020-07-22 16:30:41 +0300

    Add files via upload

---
+++ generator/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+def home(request):
+    return render(request,'generator/home.html',)
+
+def about(request):
+    return render(request,'generator/about.html',)
+
+
+def password(request):
+   thepassword = ''
+   characters = [abcdefghijklmnopqrstuvwxyz]
+   
+   if request.GET.get('uppercase'):
+       characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()?><')) 
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))         
+   
+    length = int(request.GET.get('length',12))
+
+   for i in range(length):
+       thepassword += random.choice(characters)
+
+
+
+    return render(request,'generator/password.html', {'password':thepassword})
